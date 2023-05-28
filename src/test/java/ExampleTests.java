708a854f6311f981fb7c32a1a75cb57eca6adfc2
Ela Sądel <ela@testelka.pl>
2023-05-28 17:11:31 +0200

    Screenshots on fail

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -1,52 +1,13 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
-public class ExampleTests {
-    WebDriver driver;
 
-    @BeforeEach
-    public void setup() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    public void quitDriver() {
-        driver.quit();
-    }
-
+public class ExampleTests extends BaseTest{
     @Test
-    public void screenshotExample() {
+    public void test() {
         driver.get("http://localhost:3000/");
-        File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        Path destinationPath = Paths.get("./target/image.png");
-        try {
-            Files.copy(screenshot.toPath(), destinationPath, StandardCopyOption.REPLACE_EXISTING);
-            System.out.println("Screenshot saved.");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    public void elementScreenshotExample() {
-        driver.get("http://localhost:3000/");
-        WebElement element = driver.findElement(By.cssSelector(".items-end.mx-auto"));
-        File screenshot = element.getScreenshotAs(OutputType.FILE);
-        Path destinationPath = Paths.get("./target/image.png");
-        try {
-            Files.copy(screenshot.toPath(), destinationPath, StandardCopyOption.REPLACE_EXISTING);
-            System.out.println("Screenshot saved.");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        WebElement button = driver.findElement(By.cssSelector("a.bg-primary-500"));
+        Assertions.assertEquals("Cześć!", button.getText());
     }
 }
---
+++ src/test/java/TestStatus.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+public class TestStatus implements AfterTestExecutionCallback {
+    public boolean isFailed;
+    @Override
+    public void afterTestExecution(ExtensionContext extensionContext) {
+        isFailed = extensionContext.getExecutionException().isPresent();
+    }
+}
