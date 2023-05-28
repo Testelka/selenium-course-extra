ca0dbcdef5d6bc83725df5214b4476d644672bc1
Ela Sądel <ela@testelka.pl>
2023-05-28 18:46:23 +0200

    DOM mutations

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -1,10 +1,16 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.events.DomMutationEvent;
+import org.openqa.selenium.logging.HasLogEvents;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.openqa.selenium.devtools.events.CdpEventTypes.domMutation;
 
 public class ExampleTests {
     WebDriver driver;
@@ -19,11 +25,25 @@ public class ExampleTests {
 
     @Test
     public void test() {
-        DevTools devTools = ((HasDevTools)driver).getDevTools();
-        devTools.createSession();
-        devTools.getDomains().events().addJavascriptExceptionListener(System.out::println);
-        driver.get("https://fakestore.testelka.pl/javascript-exceptions/");
-        WebElement button1 = driver.findElement(By.id("button-1"));
-        button1.click();
+        driver.get("https://fakestore.testelka.pl/zmiany-w-dom/");
+        AtomicReference<DomMutationEvent> seen = new AtomicReference<>();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        ((HasLogEvents) driver).onLogEvent(domMutation(mutation -> {
+            seen.set(mutation);
+            latch.countDown();
+        }));
+        WebElement secondButton = driver.findElement(
+                By.cssSelector("#second-button"));
+        secondButton.click();
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(
+                        latch.await(10, TimeUnit.SECONDS)),
+                () -> Assertions.assertEquals(
+                        "class" , seen.get().getAttributeName()),
+                () -> Assertions.assertEquals(
+                        "primary-button",
+                        seen.get().getCurrentValue())
+        );
     }
 }
