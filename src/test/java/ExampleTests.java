d9f0d16528679891176b30e291fed667c4f79ec5
Ela Sądel <ela@testelka.pl>
2023-06-03 12:16:08 +0200

    Network Interceptor

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -1,23 +1,22 @@
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.devtools.events.DomMutationEvent;
-import org.openqa.selenium.logging.HasLogEvents;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
+import org.openqa.selenium.devtools.NetworkInterceptor;
+import org.openqa.selenium.remote.http.HttpResponse;
+import org.openqa.selenium.remote.http.Route;
 
-import static org.openqa.selenium.devtools.events.CdpEventTypes.domMutation;
+import static org.openqa.selenium.remote.http.Contents.utf8String;
 
 public class ExampleTests {
     WebDriver driver;
+
     @BeforeEach
     public void setup() {
         driver = new ChromeDriver();
     }
+
     @AfterEach
     public void quitDriver() {
         driver.quit();
@@ -25,25 +24,12 @@ public class ExampleTests {
 
     @Test
     public void test() {
-        driver.get("https://fakestore.testelka.pl/zmiany-w-dom/");
-        AtomicReference<DomMutationEvent> seen = new AtomicReference<>();
-        CountDownLatch latch = new CountDownLatch(1);
-
-        ((HasLogEvents) driver).onLogEvent(domMutation(mutation -> {
-            seen.set(mutation);
-            latch.countDown();
-        }));
-        WebElement secondButton = driver.findElement(
-                By.cssSelector("#second-button"));
-        secondButton.click();
-        Assertions.assertAll(
-                () -> Assertions.assertTrue(
-                        latch.await(10, TimeUnit.SECONDS)),
-                () -> Assertions.assertEquals(
-                        "class" , seen.get().getAttributeName()),
-                () -> Assertions.assertEquals(
-                        "primary-button",
-                        seen.get().getCurrentValue())
-        );
+        NetworkInterceptor network = new NetworkInterceptor(driver,
+                Route.matching(req -> req.getUri().contains(".css"))
+                        .to(() -> req -> new HttpResponse().setStatus(200)
+                                .setHeader("Content-Type", "text/css")
+                                .setContent(utf8String("Nie lubię CSSa"))));
+        driver.get("https://fakestore.testelka.pl");
+        network.close();
     }
 }
