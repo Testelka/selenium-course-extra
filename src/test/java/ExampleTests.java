46c68a88b8182bfad151f07dbe80d9231095835e
Ela Sądel <ela@testelka.pl>
2023-06-03 14:39:04 +0200

    Alerts

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -1,54 +1,69 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.html5.LocalStorage;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.Set;
+import java.time.Duration;
 
 public class ExampleTests {
     WebDriver driver;
+    WebDriverWait wait;
 
     @BeforeEach
     public void setup() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.get("https://fakestore.testelka.pl/alerty/");
     }
 
     @AfterEach
     public void quitDriver() {
         driver.quit();
     }
-
     @Test
-    public void test() {
-        driver.get("https://airly.org/map/pl/#50.0626789849,19.9326583871");
-        LocalStorage storage = ((ChromeDriver)driver).getLocalStorage();
-        String map = storage.getItem("persist:map");
-        storage.removeItem("persist:map");
-        storage.setItem("some key", "some value");
-        Set<String> keys = storage.keySet();
-        int size = storage.size();
-        storage.clear();
+    public void alert() {
+        driver.findElement(By.cssSelector("[onclick='alertFunction()']")).click();
+        Alert alert = driver.switchTo().alert();
+        Assertions.assertEquals("To jest po prostu alert", alert.getText());
+        alert.accept();
     }
 
     @Test
-    public void test2() {
-        driver.navigate().to("https://airly.eu/map/pl/#50.06237,19.93898");
-        String key = "persist:map";
+    public void confirm_accept() {
+        driver.findElement(By.cssSelector("[onclick='confirmFunction()']")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        String message = driver.findElement(By.cssSelector("#demo")).getText();
+        Assertions.assertEquals("Wybrana opcja to OK!", message);
+    }
+    @Test
+    public void confirm_dismiss() {
+        driver.findElement(By.cssSelector("[onclick='confirmFunction()']")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
+        String message = driver.findElement(By.cssSelector("#demo")).getText();
+        Assertions.assertEquals("Wybrana opcja to Cancel!", message);
+    }
+    @Test
+    public void prompt() {
+        driver.findElement(By.cssSelector("[onclick='promptFunction()']")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys("Anka");
+        alert.accept();
+        String message = driver.findElement(By.cssSelector("#prompt-demo")).getText();
+        Assertions.assertEquals("Cześć Anka! Jak leci?", message);
+    }
 
-        String value = (String) ((JavascriptExecutor) driver)
-                .executeScript("return localStorage.getItem(arguments[0]);", key);
-        ((JavascriptExecutor) driver)
-                .executeScript(
-                        "localStorage.setItem(arguments[0], arguments[1]);", "some key", "some value");
-        ((JavascriptExecutor) driver)
-                .executeScript("localStorage.removeItem(arguments[0]);", key);
-        String keyByIndex = (String) ((JavascriptExecutor) driver)
-                .executeScript("return localStorage.key(arguments[0]);", 2);
-        long size = (long) ((JavascriptExecutor) driver)
-                .executeScript("return localStorage.length;");
-        ((JavascriptExecutor) driver).executeScript("localStorage.clear();");
+    @Test
+    public void alert_with_wait() {
+        driver.findElement(By.cssSelector("[onclick='delayedAlertFunction()']")).click();
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        Assertions.assertEquals("Miałem mały poślizg", alert.getText());
     }
 }
