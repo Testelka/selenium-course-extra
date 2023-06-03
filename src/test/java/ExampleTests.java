ff166c397571d4c49639735e900567470604a1aa
Ela Sądel <ela@testelka.pl>
2023-06-03 12:43:31 +0200

    Basic Auth

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -1,11 +1,16 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.HasAuthentication;
+import org.openqa.selenium.UsernameAndPassword;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.NetworkInterceptor;
 import org.openqa.selenium.remote.http.HttpResponse;
 import org.openqa.selenium.remote.http.Route;
+
+import java.net.URI;
+import java.util.function.Predicate;
 
 import static org.openqa.selenium.remote.http.Contents.utf8String;
 
@@ -24,12 +29,7 @@ public class ExampleTests {
 
     @Test
     public void test() {
-        NetworkInterceptor network = new NetworkInterceptor(driver,
-                Route.matching(req -> req.getUri().contains(".css"))
-                        .to(() -> req -> new HttpResponse().setStatus(200)
-                                .setHeader("Content-Type", "text/css")
-                                .setContent(utf8String("Nie lubię CSSa"))));
-        driver.get("https://fakestore.testelka.pl");
-        network.close();
+        ((HasAuthentication)driver).register(UsernameAndPassword.of("harrypotter", "Alohomora"));
+        driver.get("https://fakestore.testelka.pl/wp-content/uploads/protected/cos.html");
     }
 }
