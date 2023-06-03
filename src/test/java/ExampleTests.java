1a11a2b5dc3bc0743c4c80bcbb5f5bf9f808e808
Ela Sądel <ela@testelka.pl>
2023-06-03 16:00:01 +0200

    CTRL+click

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -1,10 +1,17 @@
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.webaudio.model.AudioListenerWillBeDestroyed;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.locators.RelativeLocator;
+
+import java.util.List;
 
 public class ExampleTests {
     WebDriver driver;
@@ -20,9 +27,20 @@ public class ExampleTests {
     }
     @Test
     public void test() {
-        driver.get("http://localhost:8080/product/history-of-astronomy-by-george-forbes/");
-        By quantity = By.cssSelector("input[name='quantity']");
-        By addToCart = RelativeLocator.with(By.cssSelector("button")).toRightOf(quantity);
-        driver.findElement(addToCart).click();
+        driver.navigate().to("https://fakestore.testelka.pl/select/");
+        List<WebElement> items = driver.findElements(By.cssSelector(".ui-selectee"));
+        Actions actions = new Actions(driver);
+        actions
+                .keyDown(Keys.CONTROL)
+                .click(items.get(1))
+                .click(items.get(3))
+                .click(items.get(4))
+                .keyUp(Keys.CONTROL)
+                .perform();
+
+        List<WebElement> selectedItems = driver.findElements(By.cssSelector(".ui-selected"));
+        Assertions.assertEquals(3, selectedItems.size(),
+                "Number of selected items is not what expected.");
+
     }
 }
