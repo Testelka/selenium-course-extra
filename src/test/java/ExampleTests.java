0e141dc69cd9305547da8feaed2ca5ba8ee7cb5f
Ela Sądel <ela@testelka.pl>
2023-06-03 15:19:37 +0200

    Relative locators

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -1,21 +1,17 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
+import org.openqa.selenium.support.locators.RelativeLocator;
 
 public class ExampleTests {
     WebDriver driver;
-    WebDriverWait wait;
 
     @BeforeEach
     public void setup() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
     }
 
     @AfterEach
@@ -24,16 +20,9 @@ public class ExampleTests {
     }
     @Test
     public void test() {
-        driver.get("http://localhost:8080/product/calculus-made-easy-by-silvanus-p-thompson/");
-        String originalWindows = driver.getWindowHandle();
-        driver.switchTo().newWindow(WindowType.TAB);
-        //driver.switchTo().newWindow(WindowType.WINDOW);
-        //wait.until(ExpectedConditions.numberOfWindowsToBe(2));
-        String newWindow = driver.getWindowHandle();
-        driver.get("http://localhost:8080/");
-        driver.switchTo().window(originalWindows);
-        driver.get("http://localhost:8080/wishlist/");
-        driver.switchTo().window(newWindow);
-        driver.close();
+        driver.get("http://localhost:8080/product/history-of-astronomy-by-george-forbes/");
+        By quantity = By.cssSelector("input[name='quantity']");
+        By addToCart = RelativeLocator.with(By.cssSelector("button")).toRightOf(quantity);
+        driver.findElement(addToCart).click();
     }
 }
